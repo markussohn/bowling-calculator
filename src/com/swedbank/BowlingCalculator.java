7519d6bc82f175916a8c457ccc116725ceade282
markussohn <markus.justinas@gmail.com>
2016-02-09 22:07:28 +0200

    Now returns correct score after hitting same number of pins 20 times when no spares and strikes happened.

--- src/com/swedbank/BowlingCalculator.java
+++ src/com/swedbank/BowlingCalculator.java
@@ -1,10 +1,13 @@
 package com.swedbank;
 
 public class BowlingCalculator {
+    private int totalScore = 0;
+
     public void roll(int score){
+        totalScore += score;
 
     }
     public int getResult(){
-       return 20;
+       return totalScore;
     }
 }
--- test/com/swedbank/BowlingCalculatorTest.java
+++ test/com/swedbank/BowlingCalculatorTest.java
@@ -6,14 +6,21 @@ import static org.junit.Assert.*;
 
 public class BowlingCalculatorTest {
 
+    private BowlingCalculator doRoll = new BowlingCalculator();
 
     @Test
-    public void canWeRollTwentyTimes(){
-        BowlingCalculator doRoll = new BowlingCalculator();
+    public void canWeRollOneTwentyTimes(){
         for (int i = 0; i < 20; i++) {
             doRoll.roll(1);
-            assertEquals(20, doRoll.getResult());
         }
+        assertEquals(20, doRoll.getResult());
+    }
 
+    @Test
+    public void canWeRollTwoTwentyTimes(){
+        for (int i = 0; i < 20; i++) {
+            doRoll.roll(2);
+        }
+        assertEquals(40, doRoll.getResult());
     }
 }
